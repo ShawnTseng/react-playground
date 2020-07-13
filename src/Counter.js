de85496222e36cc68cecf3dc3943590aad1fec9f
shawntseng <shawntseng40@gmail.com>
2020-07-14 07:13:28 +0800

    優化

--- src/Counter.js
+++ src/Counter.js
@@ -6,15 +6,17 @@ class Counter extends Component {
     }
 
     addCount = () => {
+        const { count } = this.state;
         this.setState({
-            count: this.state.count + 1
+            count: count + 1
         })
     }
 
     render() {
+         const { count } = this.state;
         return (
             <div>
-                <h1>{this.state.count}</h1>
+                <h1>{count}</h1>
                 <button onClick={this.addCount}>+1</button>
             </div>
         );
