ed10b60de9afe89d689739732f8db865f0b4c190
shawntseng <shawntseng40@gmail.com>
2020-07-17 07:30:46 +0800

    加入input

---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Enter you name</h3>
+                <input type="text" />
+            </div>
+        );
+    }
+}
+
+export default Input;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Counter from './Counter';
+import Input from './Input';
 
-render(<Counter />, document.getElementById('root'));
+render(<div><Counter /><Input /></div>, document.getElementById('root'));
 
